2cd65636c13c737e090443970afa6d714f935b82
Abhiram Bollina <abhibol@unc.edu>
2025-08-02 19:12:57 -0400

    Added comparison between black scholes and market price

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ if st.button("Submit"):
             st.session_state["ticker_submitted"] = True
             st.session_state["r"] = float(yf.Ticker("^TNX").history(period="1d")["Close"].iloc[-1]/100)
             st.session_state["sig"] = bsm.historical_volatility(yf.download(st.session_state["ticker"], period="6mo", interval="1d"))
+            
     else:
         st.error("Please enter a valid ticker.")
 
@@ -55,23 +56,40 @@ if st.session_state["ticker_submitted"] and st.session_state["data"] is not None
     st.subheader(f"Closing Price for {st.session_state['ticker'].upper()} (Last 30 Days)")
 
     st.pyplot(scht.stock_plot_30d(st.session_state["ticker"], st.session_state["data"]))
+    st.subheader(f"Current Closing Price: ${float(st.session_state["data"]["Close"].iloc[-1]):.2f}")
 
-    st.subheader("Black-Scholes Option Pricing")
+    st.header("Black-Scholes Option Pricing")
     
     option_type_input = st.selectbox("Select Option Type:", options=["Call", "Put"])
+
+    
 
     S = float(st.session_state["data"]["Close"].iloc[-1])
     r = st.session_state["r"]
     sig = st.session_state["sig"]
-    K_input = st.number_input("Strike Price (K):", value=S, step=2.5)
-    T_date = st.date_input("Time to Maturity (T):", value=datetime.today() + timedelta(days=30)) 
-    T_input = (datetime(T_date.year, T_date.month, T_date.day) - datetime.today()).days / 365.0  # Convert days to years
+    
+    ticker = st.session_state["ticker"]
+
+    expiry_options = yf.Ticker(ticker).options
+    expiry_date = st.selectbox("Select Expiry Date:", options=expiry_options)
+
+    strike_price_options = []
+    chain = yf.Ticker(ticker).option_chain(expiry_date)
+    if option_type_input == "Call":
+        strike_price_options = chain.calls['strike'].tolist()
+    else:
+        strike_price_options = chain.puts['strike'].tolist()
+
+    closest_strike_index = strike_price_options.index(min(strike_price_options, key=lambda x: abs(x - S)))
+    
+    T = (datetime.strptime(expiry_date, "%Y-%m-%d") - datetime.today()).days / 365.0 
+    K = st.selectbox("Select Strike Price:", options=strike_price_options, index=closest_strike_index)
 
     if st.button("Calculate Option Price"):
-        if K_input >= 0 and T_input > 0 and option_type_input in ["Call", "Put"]:
+        if K and T and option_type_input:
             st.session_state["option_submitted"] = True
-            st.session_state["K"] = K_input
-            st.session_state["T"] = T_input
+            st.session_state["K"] = K
+            st.session_state["T"] = T
             st.session_state["option_type"] = option_type_input
         else:
             st.error("Invalid input")
@@ -82,11 +100,15 @@ if st.session_state["option_submitted"]:
     T = float(st.session_state["T"])
     option_type = st.session_state["option_type"]
     if option_type == "Call":
+        call_row = chain.calls[chain.calls["strike"] == K]
         option_price = bsm.bs_call(S, K, T, r, sig)
-        st.success(f"The price of the Call Option is: ${option_price:.2f} per share")
+        st.success(f"The price of the Black-Scholes Call Option is: ${option_price:.2f} per share")
+        st.write(f"Market Mid Price of Call Option: {(call_row["bid"].values[0] + call_row["ask"].values[0]) / 2:.2f} per share")
     else:
+        put_row = chain.puts[chain.puts["strike"] == K]
         option_price = bsm.bs_put(S, K, T, r, sig)
-        st.success(f"The price of the Put Option is: ${option_price:.2f} per share")
+        st.success(f"The price of the Black-Scholes Put Option is: ${option_price:.2f} per share")
+        st.write(f"Market Mid Price of Put Option: {(put_row['bid'].values[0] + put_row['ask'].values[0]) / 2:.2f} per share")
     
 
     
